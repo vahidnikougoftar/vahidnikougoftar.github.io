273f6a49576004707652a633ae0a199533e9d9b3
Vahid Nikougoftar <vahid.nikougoftar@gmail.com>
2024-12-18 16:51:54 +0000

    deployed website

---
+++ kidslab/src/App.js
@@ -0,0 +1,25 @@
+import logo from './image.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to the AI Kids Lab!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Embrace Technology, Enlighten your future
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
